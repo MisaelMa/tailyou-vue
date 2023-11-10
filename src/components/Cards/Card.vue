9477118101d4c093a724590c0135ab42acf52e9b
Jonyuwu <jonathanjesusabrajanhau@gmail.com>
2023-11-10 17:25:36 -0500

    feat(dashboard):Vue-Components

--- src/components/Cards/Card.vue
+++ src/components/Cards/Card.vue
@@ -1,33 +1,18 @@
 <template>
   <div :class="card({ variant, size })">
-    <div>
-      <img
-        :src="imageUrl"
-        :class="cardImage({ variantImage, sizeImage })"
-      />
-    </div>
-
-    <div :class="cardTitle({ variantTitle, sizeTitle })">
-      {{ title }}
-    </div>
-
-    <slot></slot>
+    <slot> </slot>
   </div>
 </template>
 
 <script setup lang="ts">
+import { cva, type VariantProps } from "class-variance-authority";
+import { defineProps } from "vue";
 import { card } from "@tailyou/cva";
-import { cardImage } from "@tailyou/cva";
-import { cardTitle } from "@tailyou/cva";
 
-const { variant, size, imageUrl, title } = defineProps<{
-  variant?: any;
-  size?: any;
-  variantImage?: any;
-  sizeImage?: any;
-  variantTitle?: any;
-  sizeTitle?: any;
-  imageUrl: string;
-  title: string;
+type PropsCardGeneral = VariantProps<typeof card>;
+
+defineProps<{
+  variant?: PropsCardGeneral["variant"];
+  size?: PropsCardGeneral["size"];
 }>();
 </script>
---
+++ src/components/SpanPoint/T-spanPoint.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-for="(item, index) in data" :key="index" class="mb-3">
+    <div :class="spanPoint({ variant: variantSpan })">
+      <span
+        :class="`bg-${item.color}`"
+        class="w-2.5 h-2.5 rounded-full mt-1 justify-center items-center"
+      ></span>
+      <div>
+        <span class="text-sm"> {{ item.value }} {{ item.text }} </span>
+      </div>
+    </div>
+  </div>
+  <div :class="lineSpace({ variant: variantLine })"></div>
+</template>
+
+<script setup lang="ts">
+import { cva, type VariantProps } from "class-variance-authority";
+import { defineProps } from "vue";
+import { lineSpace, spanPoint } from "@tailyou/cva";
+
+type PropsLine = VariantProps<typeof lineSpace>;
+type PropsSpanPoint = VariantProps<typeof spanPoint>;
+
+defineProps<{
+  variantLine?: PropsLine["variant"];
+  variantSpan?: PropsSpanPoint["variant"];
+  data: DataSpan[];
+}>();
+type DataSpan = {
+  text: string;
+  color: string;
+  value?: number;
+};
+</script>
