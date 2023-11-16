80cb15b58b53a153f92ece87de96e1f28598d361
Jonyuwu <jonathanjesusabrajanhau@gmail.com>
2023-11-15 20:14:52 -0500

    (chip.cva):new cva for chip

---
+++ src/components/Chips/T-chip.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="chip({ variant, size })">
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+import { chip } from "@tailyou/cva";
+import { cva, type VariantProps } from "class-variance-authority";
+import { defineProps } from "vue";
+
+type PropsChip = VariantProps<typeof chip>;
+defineProps<{
+  variant?: PropsChip["variant"];
+  size?: PropsChip["size"];
+}>();
+</script>
