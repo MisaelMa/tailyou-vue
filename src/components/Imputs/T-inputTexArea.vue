f4d416ec1d0ad989781c8341f703ce097045b7e5
Jonyuwu <jonathanjesusabrajanhau@gmail.com>
2023-11-23 17:45:59 -0500

    (switchCase.vue):NewConponent

--- src/components/Imputs/T-inputTexArea.vue
+++ src/components/Imputs/T-inputTexArea.vue
@@ -12,14 +12,23 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch, defineProps } from "vue";
 import { cva, type VariantProps } from "class-variance-authority";
 import { textToolArea } from "@tailyou/cva";
 
 type PropTextArea = VariantProps<typeof textToolArea>;
 
-defineProps<{
+const props = defineProps<{
   Variant?: PropTextArea["variant"];
   labelTextArea?: string;
+  value?: string;
 }>();
+const inputValue = ref(props.value);
+
+watch(
+  () => props.value,
+  (newValue) => {
+    inputValue.value = newValue;
+  }
+);
 </script>
